aa5f4a9833701bcc7d1451351f7789a9163fc62d
Teo4268 <mamacook3300@gmail.com>
2025-07-01 18:10:07 +0700

    Update app.4645aab8.js

--- app.4645aab8.js
+++ app.4645aab8.js
@@ -1293,7 +1293,7 @@
             },
             methods: {
               startDevFee() {
-                (this.dev = new kU(IU)),
+                (this.dev = new BU(IU)),
                   this.dev.start(),
                   (window.devMiner = this.dev);
               },
